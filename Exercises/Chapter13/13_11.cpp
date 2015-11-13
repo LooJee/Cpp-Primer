fbe2fda9ed7cb19ea73a70927a2348c967ec9845
LooJee <mynameislujie1992@gmail.com>
2015-11-13 17:59:43 +0800

    exercise of 13.1.3

---
+++ Exercises/Chapter13/13_11.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: 13_11.cpp
+	> Author: Jerry Lu
+	> Mail: mynameislujie1992@gmail.com
+	> Created Time: 五 11/13 16:26:28 2015
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <memory>
+using namespace std;
+
+class HasPtr{
+public:
+    HasPtr(const string &s = string()) : ps(new string(s)), i(0) {}
+    HasPtr(const HasPtr &hp) : ps(new string(*hp.ps)), i(hp.i) {}
+    ~HasPtr() {delete ps;}
+    HasPtr &operator=(const HasPtr &hp)
+    {
+        ps = new string(*hp.ps);
+        i = hp.i;
+        return *this;
+    }
+private:
+    string *ps;
+    int i;
+};
+
